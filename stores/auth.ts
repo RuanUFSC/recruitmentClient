6f08db69b0d96e007487a27d3173f4e9449acb3a
Ruan Lopes <90937468+RuanUFSC@users.noreply.github.com>
2024-10-14 14:27:21 -0300

    Update auth.ts

--- stores/auth.ts
+++ stores/auth.ts
@@ -24,7 +24,7 @@ export const useAuthStore = defineStore('auth', {
         };
         const translatedRole = roleTranslation[role] || role;
 
-        await $fetch('http://recruitmentapi.onrender.com/api/register', {
+        await $fetch('https://recruitmentapi.onrender.com/api/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ export const useAuthStore = defineStore('auth', {
         this.usuario = user
         const token = await user.getIdToken()
         this.token = token
-        const situacao = await $fetch('http://recruitmentapi.onrender.com/api/auth', {
+        const situacao = await $fetch('https://recruitmentapi.onrender.com/api/auth', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
